c66bec9dff1293b68d81e4566d487881ff9da720
Seven <76436489+Zetonen@users.noreply.github.com>
2025-12-07 09:14:42 +0200

    change cors

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,14 @@ import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  app.enableCors({
+    // origin: ['http://localhost:5173', '*'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+    allowedHeaders:
+      'Content-Type, Accept, Authorization, ngrok-skip-browser-warning',
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -13,6 +21,6 @@ async function bootstrap() {
   );
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
