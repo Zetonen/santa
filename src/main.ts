c39479c509b1473faa8fa5f583a723828b816a72
Seven <76436489+Zetonen@users.noreply.github.com>
2025-12-09 23:06:54 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['*'],
+    origin: ['https://santa-app-git-main-sevens-projects-8100cb6f.vercel.app'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
     allowedHeaders:
